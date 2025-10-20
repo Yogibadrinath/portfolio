7d65e20677d4e67c4d6c7d710d92ec62a35b04e7
yogibadrinath <yogibadrinatha@quberootcorp.com>
2025-10-20 13:03:12 +0530

    favicon-title

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -10,7 +10,7 @@ const About: React.FC = () => {
       <ul className="list-inline">
         <li className="list-inline-item"><b>Email:</b> <a href="mailto:yogibadrinath001@gmail.com">yogibadrinath001@gmail.com</a></li>
         <li className="list-inline-item ms-3"><b>Phone:</b> +91 9600591834</li>
-        <li className="list-inline-item ms-3"><b>Portfolio:</b> <a href="https://yogibadrinath.github.io/portfolio/" target="_blank" rel="noreferrer">View</a></li>
+        {/* <li className="list-inline-item ms-3"><b>Portfolio:</b> <a href="https://yogibadrinath.github.io/portfolio/" target="_blank" rel="noreferrer">View</a></li> */}
       </ul>
     </section>
   );
